883c833c87526e3464890e942fde5d1027955ed5
MikaelSantos12 <mikaelsantos120@outlook.com>
2023-10-01 14:26:04 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,29 @@
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Hero, Preview } from "./styles";
+
+import Image from "next/image";
+import previewImage from '../../assets/app-review.svg';
+
+export default function Home() {
+    return (
+        <Container>
+            <Hero>
+                <Heading size='4xl'>
+                    Agendamento descomplicado
+                </Heading>
+                <Text size='lg'>
+                    Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+                </Text>
+            </Hero>
+            <Preview>
+                <Image
+                    src={previewImage}
+                    height={480}
+                    quality={100}
+                    priority
+                    alt='Calendario simbolizando a aplicação em funcionamento '
+                />
+            </Preview>
+        </Container>
+    )
+}
